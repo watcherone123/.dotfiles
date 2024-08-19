1c56b1563912e8df17b1ad033ad1ebda63799476
watcher <watcherone123@gmail.com>
2024-08-19 20:49:42 +0800

    nvim: Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,5 @@ vim.g.loaded_ruby_provider = 0 -- Disable ruby provider
 vim.g.loaded_node_provider = 0 -- Disable node provider
 vim.opt.shell = "nu"
 vim.g.lazyvim_python_lsp = "pyright"
+vim.opt.list = true
+vim.opt.listchars:append("space:.")
